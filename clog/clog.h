a0329fb7986f57f1b8a85ba66b8d958f8aebac36
王渊鸥 <wangyuanou@foxmail.com>
2017-03-30 15:52:17 +0800

    Initial Commit

---
+++ clog/clog.h
@@ -0,0 +1,19 @@
+//
+//  clog.h
+//  clog
+//
+//  Created by 王渊鸥 on 2017/3/30.
+//  Copyright © 2017年 王渊鸥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for clog.
+FOUNDATION_EXPORT double clogVersionNumber;
+
+//! Project version string for clog.
+FOUNDATION_EXPORT const unsigned char clogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <clog/PublicHeader.h>
+
+
